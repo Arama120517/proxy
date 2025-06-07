258ccd30fee7ee6d1381111d0e8746da13b84fb5
Arama <arama.dev@outlook.com>
2025-06-07 17:40:17 +0800

    feat: 添加简略状态

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import json
+import os
+from dataclasses import dataclass
 from pathlib import Path
 
 from src import BaseSource
@@ -11,23 +13,49 @@ CURRENT_DIR_PATH: Path = Path().cwd()
 SRC_DIR_PATH: Path = CURRENT_DIR_PATH / 'src'
 
 
+@dataclass
+class Status:
+    success: bool
+    tag_count: int
+
+
 def main() -> None:
     with open(SRC_DIR_PATH / 'template.json', 'r', encoding='utf-8') as f:
         template: dict = json.loads(f.read())
 
     servers: list[str] = []
+    status: dict[str, Status] = {}
     for source in SOURCES:
-        for outbound in source.get_outbounds():
-            # 防止重复
-            if outbound['server'] in servers:
-                continue
+        name = source.__class__.__name__
+        status[name] = Status(False, 0)
+        try:
+            for outbound in source.get_outbounds():
+                # 防止重复
+                if outbound['server'] in servers:
+                    continue
 
-            tag: str = outbound['tag']
-            template['outbounds'][0]['outbounds'].append(tag)
-            template['outbounds'][1]['outbounds'].append(tag)
-            template['outbounds'].insert(-3, outbound)
+                tag: str = outbound['tag']
+                template['outbounds'][0]['outbounds'].append(tag)
+                template['outbounds'][1]['outbounds'].append(tag)
+                template['outbounds'].insert(-3, outbound)
 
-            servers.append(outbound['server'])
+                servers.append(outbound['server'])
+
+                status[name].tag_count += 1
+            status[name].success = True
+        except Exception:
+            continue
+
+    with open(os.environ.get('GITHUB_STEP_SUMMARY'), 'w', encoding='utf-8') as f:
+        f.write(f"""## 结果
+
+- **节点总数**: {len(template['outbounds']) - 5}
+
+| 名称 | 状态 | 节点数量 |
+| ---- | ---- | -------- |
+""")
+        for name, stat in status.items():
+            f.write(f'| {name} | {"✅" if stat.success else "❌"} | {stat.tag_count} |\n')
 
     with open(CURRENT_DIR_PATH / 'result.json', 'w', encoding='utf-8') as f:
         f.write(json.dumps(template, indent=4, ensure_ascii=False))
