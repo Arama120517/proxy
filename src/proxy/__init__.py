2c508f14555c159e594f5c5932cf6e2ea55c72e7
Arama120517 <arama.dev@outlook.com>
2026-03-10 01:08:27 +0800

    fix: 修复目录不对的问题

--- src/proxy/__init__.py
+++ src/proxy/__init__.py
@@ -27,8 +27,10 @@ def get_session() -> Session:
 def load_result() -> OutBounds:
     outbounds: OutBounds = []
 
-    for result_dir in [f for f in Path('./results').iterdir() if f.is_dir()]:
-        with (result_dir / 'result.json').open('r', encoding='utf-8') as f:
+    for result_file in [
+        f for f in Path('./results').iterdir() if f.is_file() and f.suffix == '.json'
+    ]:
+        with result_file.open('r', encoding='utf-8') as f:
             outbounds += json.load(f)
 
     return outbounds
@@ -41,35 +43,3 @@ def dump_result(outbounds: OutBounds) -> None:
             if 'server' in outbound:
                 results.append(outbound)
         json.dump(results, f, ensure_ascii=False)
-
-
-# class BaseSource(ABC, Session):
-#     logger: logging.Logger
-
-#     def __init__(self):
-#         super().__init__()
-
-#         self.headers.update({'User-Agent': generate().text})
-#         self.logger = logging.getLogger(self.__class__.__name__)
-
-#     def main(self) -> NoReturn:
-#         """运行程序"""
-
-#         from sys import exit
-
-#         from rich import print_json
-
-#         try:
-#             servers = self.get_outbounds()
-#             print_json(data=servers, indent=4, ensure_ascii=False)
-#         except Exception:
-#             self.logger.exception('获取失败')
-
-#         exit(0)
-
-#     @abstractmethod
-#     def get_outbounds(self) -> OutBounds: ...
-
-#     @staticmethod
-#     def extract_proxy_servers(config: dict) -> OutBounds:
-#         return [o for o in config.get('outbounds', []) if o.get('server')]
